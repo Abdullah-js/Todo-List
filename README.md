00e5b70c9fc4131a3625acfed60e393221894d99
Black Wolf <67378732+Abdullah-js@users.noreply.github.com>
2020-11-07 20:19:56 +0300

    Todo List
    I is usefull

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+```bash
+# Install dependencies
+npm install
+
+# Serve on localhost:3000
+npm start
+
+# Build for production
+npm run build
+```
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>blah blah blah</h1>
+      <p>react is awesome </p>
+    
+    </React.Fragment>
+  )
+}
+
+
+export default About;
